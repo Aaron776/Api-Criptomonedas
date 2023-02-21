91e32297cc7515ec1e61df80728d0063c62c1f78
Aaron776 <aronortiz90@yahoo.com>
2023-02-20 19:12:55 -0500

    Actualizado

---
+++ app.js
@@ -0,0 +1,16 @@
+let endpoint='https://api.binance.com/api/v3/ticker/price';
+fetch(endpoint).then(respuesta=>respuesta.json()).then(data=>{
+    mostrarData(data)
+}).catch(error=>{
+    console.log(error)
+})
+
+const mostrarData=(data)=>{
+    console.log(data)
+    let body = ''
+    for (let i=0; i < data.length; i++) {
+        body += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
+    }
+    document.getElementById('registros').innerHTML = body
+}
+
